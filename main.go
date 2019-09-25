ef833edaac47b3dae52f71ee699b1ff713dc613b
Gustav Paul <gpaul@mesosphere.io>
2019-09-25 12:38:39 -0700

    Update main.go

--- main.go
+++ main.go
@@ -51,6 +51,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: %s\n", Version)
 	fmt.Fprintf(w, "Build time: %s\n", Buildtime)
-	fmt.Fprintf(w, "Beautiful day, today!\n")
+	fmt.Fprintf(w, "Let's try that again ^_^\n")
 }
 // [END all]
